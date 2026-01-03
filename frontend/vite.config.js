0965593f446eb9194dea18e14d2d412bcf37e1dd
tanaka <nakatanakatana@gmail.com>
2026-01-03 17:23:09 +0900

    add vitest browser mode

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
+import { playwright } from '@vitest/browser-playwright'
 import devtools from 'solid-devtools/vite';
 
 // https://vitejs.dev/config/
@@ -16,8 +17,32 @@ export default defineConfig({
     },
   },
   test: {
-    environment: 'jsdom',
-    setupFiles: ['./src/vitest-setup.ts'],
-    globals: true,
+    projects: [
+      {
+        name: "unit",
+        test: {
+          environment: 'jsdom',
+          setupFiles: ['./src/vitest-setup.ts'],
+          globals: true,
+          include: [
+            'src/**/*.{test,spec}.ts',
+          ],
+        },
+      },
+      {
+        name: 'browser',
+        test: {
+          include: [
+            'tests/browser/**/*.{test,spec}.ts',
+            'tests/**/*.browser.{test,spec}.ts',
+          ],
+        },
+        browser: {
+          enabled: true,
+          provider: playwright(),
+          instances: [],
+        },
+      },
+    ],
   },
 });
