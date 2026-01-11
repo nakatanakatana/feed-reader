93d4c81a3fa07bf075dfed0ea8acd58b17f703be
tanaka <nakatanakatana@gmail.com>
2026-01-11 17:05:38 +0900

    move frontend sources

--- panda.config.ts
+++ panda.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   preflight: true,
 
   // Where to look for your css declarations
-  include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+  include: ["./frontend/src/**/*.{js,jsx,ts,tsx}", "./frontend/pages/**/*.{js,jsx,ts,tsx}"],
 
   // Files to exclude
   exclude: [],
@@ -16,5 +16,5 @@ export default defineConfig({
   },
 
   // The output directory for your css system
-  outdir: "styled-system",
+  outdir: "frontend/styled-system",
 });
--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import solid from "vite-plugin-solid";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: "frontend",
   plugins: [devtools(), tanstackRouter({ target: "solid" }), solid()],
   build: {
     outDir: "dist/frontend",
