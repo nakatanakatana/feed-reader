174e9da846db01b23ee6a9f172d361637fe65887
tanaka <nakatanakatana@gmail.com>
2026-01-26 22:17:54 +0900

    feat(api): Implement sorting for ListTags

--- cmd/feed-reader/tag_service_test.go
+++ cmd/feed-reader/tag_service_test.go
@@ -3,9 +3,14 @@ package main
 import (
 	"context"
 	"testing"
+	"time"
 
 	"connectrpc.com/connect"
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
+
 	tagv1 "github.com/nakatanakatana/feed-reader/gen/go/tag/v1"
 	"github.com/nakatanakatana/feed-reader/store"
 )
@@ -14,7 +19,6 @@ func TestTagServer(t *testing.T) {
 	ctx := context.Background()
 	_, db := setupTestDB(t)
 	s := store.NewStore(db)
-	// NewTagServer is not implemented yet, this will fail to compile.
 	handler := NewTagServer(s, nil)
 
 	t.Run("Create and List Tags", func(t *testing.T) {
@@ -72,5 +76,42 @@ func TestTagServer(t *testing.T) {
 				t.Errorf("deleted tag still found in ListTags")
 			}
 		}
+	})
+}
+
+func TestTagServer_ListTags_Sorting(t *testing.T) {
+	ctx := context.Background()
+	_, db := setupTestDB(t)
+	s := store.NewStore(db)
+	handler := NewTagServer(s, nil)
+
+	// Create Tag 1
+	res1, err := handler.CreateTag(ctx, connect.NewRequest(&tagv1.CreateTagRequest{Name: "Tag 1"}))
+	require.NoError(t, err)
+
+	time.Sleep(1100 * time.Millisecond)
+
+	// Create Tag 2
+	res2, err := handler.CreateTag(ctx, connect.NewRequest(&tagv1.CreateTagRequest{Name: "Tag 2"}))
+	require.NoError(t, err)
+
+	t.Run("Sort Descending True", func(t *testing.T) {
+		req := &tagv1.ListTagsRequest{SortDescending: proto.Bool(true)}
+		res, err := handler.ListTags(ctx, connect.NewRequest(req))
+		require.NoError(t, err)
+
+		require.Len(t, res.Msg.Tags, 2)
+		assert.Equal(t, res2.Msg.Tag.Id, res.Msg.Tags[0].Id)
+		assert.Equal(t, res1.Msg.Tag.Id, res.Msg.Tags[1].Id)
+	})
+
+	t.Run("Sort Descending False", func(t *testing.T) {
+		req := &tagv1.ListTagsRequest{SortDescending: proto.Bool(false)}
+		res, err := handler.ListTags(ctx, connect.NewRequest(req))
+		require.NoError(t, err)
+
+		require.Len(t, res.Msg.Tags, 2)
+		assert.Equal(t, res1.Msg.Tag.Id, res.Msg.Tags[0].Id)
+		assert.Equal(t, res2.Msg.Tag.Id, res.Msg.Tags[1].Id)
 	})
 }
