5f492ccffd2b437fc6e13a639e3fda907d0a3857
nakatanakatana <nakatanakatana@gmail.com>
2026-01-28 23:43:39 +0900

    migrate db on run (#75)
    * feat: add automatic database migration using sqldef
    
    * fix: handle db.Close error in migration
    
    * update schema.sql

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/hashicorp/go-retryablehttp v0.7.8
 	github.com/mattn/go-sqlite3 v1.14.33
 	github.com/mmcdole/gofeed v1.3.0
+	github.com/sqldef/sqldef/v3 v3.9.4
 	github.com/stretchr/testify v1.11.1
 	golang.org/x/net v0.49.0
 	google.golang.org/protobuf v1.36.11
@@ -19,13 +20,26 @@ require (
 	github.com/PuerkitoBio/goquery v1.8.0 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/goccy/go-yaml v1.19.1 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mmcdole/goxpp v1.1.1-0.20240225020742-a0c311522b23 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/ncruces/go-strftime v1.0.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
+	golang.org/x/exp v0.0.0-20251125195548-87e1e737ad39 // indirect
+	golang.org/x/sync v0.19.0 // indirect
+	golang.org/x/sys v0.40.0 // indirect
 	golang.org/x/text v0.33.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	modernc.org/libc v1.67.1 // indirect
+	modernc.org/mathutil v1.7.1 // indirect
+	modernc.org/memory v1.11.0 // indirect
+	modernc.org/sqlite v1.41.0 // indirect
 )
---
+++ sql/migration.go
@@ -0,0 +1,64 @@
+package schema
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/sqldef/sqldef/v3/database"
+	"github.com/sqldef/sqldef/v3/database/sqlite3"
+	"github.com/sqldef/sqldef/v3/parser"
+	sqlschema "github.com/sqldef/sqldef/v3/schema"
+)
+
+// Migrate performs database migration using sqldef.
+// If dryRun is true, it only detects differences and returns an error if any are found.
+func Migrate(ctx context.Context, dbPath string, desiredSchema string, dryRun bool) error {
+	config := database.Config{
+		DbName: dbPath,
+	}
+
+	db, err := sqlite3.NewDatabase(config)
+	if err != nil {
+		return fmt.Errorf("failed to open sqldef database: %w", err)
+	}
+	defer func() {
+		_ = db.Close()
+	}()
+
+	sqlParser := database.NewParser(parser.ParserModeSQLite3)
+
+	currentDDLs, err := db.ExportDDLs()
+	if err != nil {
+		return fmt.Errorf("failed to export current DDLs: %w", err)
+	}
+
+	generatorConfig := database.GeneratorConfig{}
+
+	ddls, err := sqlschema.GenerateIdempotentDDLs(sqlschema.GeneratorModeSQLite3, sqlParser, desiredSchema, currentDDLs, generatorConfig, "")
+	if err != nil {
+		return fmt.Errorf("failed to generate idempotent DDLs: %w", err)
+	}
+
+	if len(ddls) == 0 {
+		return nil
+	}
+
+	if dryRun {
+		var sb strings.Builder
+		sb.WriteString("database schema is out of sync:\n")
+		for _, ddl := range ddls {
+			sb.WriteString(ddl)
+			sb.WriteString(";\n")
+		}
+		return fmt.Errorf("%s", sb.String())
+	}
+
+	// Actually apply DDLs
+	err = database.RunDDLs(db, ddls, "", "", database.StdoutLogger{})
+	if err != nil {
+		return fmt.Errorf("failed to run DDLs: %w", err)
+	}
+
+	return nil
+}
