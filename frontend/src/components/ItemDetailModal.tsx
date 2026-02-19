c722e2a8ce934d05fe063ba54602c801d3d1dbca
tanaka <nakatanakatana@gmail.com>
2026-02-19 23:48:47 +0900

    refactor(ui): Move Published/Received icons to a shared component

--- frontend/src/components/ItemDetailModal.tsx
+++ frontend/src/components/ItemDetailModal.tsx
@@ -16,41 +16,7 @@ import { MarkdownRenderer } from "./MarkdownRenderer";
 import { ActionButton } from "./ui/ActionButton";
 import { Modal } from "./ui/Modal";
 
-const PublishedIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="14"
-    height="14"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    aria-hidden="true"
-  >
-    <path d="m22 2-7 20-4-9-9-4Z" />
-    <path d="M22 2 11 13" />
-  </svg>
-);
-
-const ReceivedIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="14"
-    height="14"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    aria-hidden="true"
-  >
-    <circle cx="12" cy="12" r="10" />
-    <polyline points="12 6 12 12 16 14" />
-  </svg>
-);
+import { PublishedIcon, ReceivedIcon } from "./ui/Icons";
 
 interface ItemDetailModalProps {
   itemId: string | undefined;
